5406bf20ae3eac83721d7ce40c010713c8bfe2e7
Robert Stupp <snazy@snazy.de>
2022-01-22 09:07:12 +0100

    [build] Fix (C) header year-check
    Fixes #88

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The Authors of CEL-Java
+ * Copyright (C) 2022 The Authors of CEL-Java
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -242,7 +242,7 @@ allprojects {
 spotless {
   kotlinGradle {
     ktfmt().googleStyle()
-    // licenseHeaderFile(rootProject.file("gradle/license-header-java.txt"), "")
+    licenseHeaderFile(rootProject.file("gradle/license-header-java.txt"), "")
   }
 }
 
